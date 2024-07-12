f9961530b1b01f87fd889474643528f473230e52
Pablo Orviz <orviz@ifca.unican.es>
2024-07-12 12:05:28 +0200

    Add JePL file structure

--- .sqa/script.gimpy-auburn-woodlouse.sh
+++ .sqa/script.gimpy-auburn-woodlouse.sh
@@ -5,13 +5,13 @@
 
 (
 cd github.com/HPCI-Lab/yProv &&
-    python3 -m pytest -v&&
-    docker stop web&&
-    docker stop db&&
-    docker rm web&&
-    docker rm db&&
-    docker network rm yprov_net&&
-    docker volume rm neo4j_data&&
-    docker volume rm neo4j_logs&&
-    docker volume rm yprov_data
+    pytest&&
+    stop web&&
+    stop db&&
+    rm web&&
+    rm db&&
+    network rm yprov_net&&
+    volume rm neo4j_data&&
+    volume rm neo4j_logs&&
+    volume rm yprov_data
 )
--- .sqa/script.sickly-wheat-wallaby.sh
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
-#
-# SPDX-License-Identifier: GPL-3.0-only
-
-(
-cd github.com/HPCI-Lab/yProv &&
-    find_doc_files.py --file_type all
-)
