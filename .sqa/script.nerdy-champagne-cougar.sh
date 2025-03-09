eab6bb19c2f63dd8658440d42a0f84720fe112e0
Pablo Orviz <orviz@ifca.unican.es>
2025-03-09 10:36:14 +0100

    Add JePL file structure

--- .sqa/script.nerdy-champagne-cougar.sh
+++ .sqa/script.nerdy-champagne-cougar.sh
@@ -5,5 +5,5 @@
 
 (
 cd github.com/HPCI-Lab/yProv &&
-    hadolint Dockerfile --failure-threshold error
+    hadolint Dockerfile SQA/Dockerfile SQA/hpci-yprov:mocktest/Dockerfile SQA/hpci-yprov:1.3/Dockerfile --failure-threshold error
 )
